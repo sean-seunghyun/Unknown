7112e90e793ae74b6623902f065d5d8b2268fba9
sean <flyseankim93@gmail.com>
2022-09-23 10:58:45 +0900

    Initial Commit

---
+++ Unknown/UnknownApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnknownApp.swift
+//  Unknown
+//
+//  Created by sean on 2022/09/23.
+//
+
+import SwiftUI
+
+@main
+struct UnknownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
